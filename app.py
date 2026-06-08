f93c1d30a2dbb60fac00f4a4922b30d02e88aac4
gangulyAkash <146986594+gangulyAkash@users.noreply.github.com>
2026-06-09 00:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model = joblib.load("strength_model.pkl")
+model = joblib.load("strength_model .pkl")
 
 st.title("AI Structural Health Assessment System")
 
